e6716d937b131f49d6801454cbdb2a36850c9b53
rpallas92 <ricardo.pallas@adidas-group.com>
2017-12-09 15:21:53 +0100

    Deleted unused files

--- FunctionalSwiftArchitectureTests/JokesNetworkDataSourceTests.swift
+++ FunctionalSwiftArchitectureTests/JokesNetworkDataSourceTests.swift
@@ -10,15 +10,6 @@ import XCTest
 
 class JokesNetworkDataSourceTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
     
     func testFetchRandomJoke() {
         let expect = expectation(description: "Fetch random joke")
