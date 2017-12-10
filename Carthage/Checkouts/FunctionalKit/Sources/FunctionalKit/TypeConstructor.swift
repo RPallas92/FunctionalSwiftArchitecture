d8b82fc633e19ad0c8e964c3cf2f8978a46fb152
rpallas92 <ricardo.pallas@adidas-group.com>
2017-12-10 16:58:09 +0100

    Added Moya framework

---
+++ Carthage/Checkouts/FunctionalKit/Sources/FunctionalKit/TypeConstructor.swift
@@ -0,0 +1,25 @@
+public protocol TypeConstructor {
+	associatedtype ParameterType
+}
+
+// Blueprint
+
+// MARK: - Definiton
+
+// MARK: - Data
+
+// MARK: - Concrete
+
+// MARK: - Equatable
+
+// MARK: - Functor
+
+// MARK: - Cartesian
+
+// MARK: - Applicative
+
+// MARK: - Traversable
+
+// MARK: - Monad
+
+// MARK: - Utility
