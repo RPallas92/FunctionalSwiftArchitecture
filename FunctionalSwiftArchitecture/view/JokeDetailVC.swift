f25591aefcb5cd4e63d46897599196411683210e
rpallas92 <ricardo.pallas@adidas-group.com>
2017-12-12 11:24:47 +0100

    Removed unused code

--- FunctionalSwiftArchitecture/view/JokeDetailVC.swift
+++ FunctionalSwiftArchitecture/view/JokeDetailVC.swift
@@ -24,14 +24,9 @@ class DetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         configureView()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
     var detailItem: NSDate? {
         didSet {
